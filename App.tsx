bacaa896e3fbcaf950180cfd589febce6947c808
Parth <parthkapatel567@gmail.com>
2024-06-10 18:31:28 +0530

    Todo

--- App.tsx
+++ App.tsx
@@ -1,81 +1,129 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type { PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Image,
-  TextInput
-} from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const TODO = () => {
 
-  const App = () => {
-    const text = 'I am parth';
-    return (
-      <View style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-      }}>
-      <Text>Some text</Text>
-      <View>
-        <Text>{text}</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-      
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        placeholder='Please Enetr a text'
-        // defaultValue="Please Enter your Input"
-      />
-      </View>
+  const Data = [
+    {
+      "id": "1",
+      "Todo": "My Data",
+    },
+    {
+      "id": "2",
+      "Todo": "My Data",
+    },
+    {
+      "id": "3",
+      "Todo": "My Data",
+    },
+    {
+      "id": "4",
+      "Todo": "My Data",
+    },
+    {
+      "id": "1",
+      "Todo": "My Data",
+    },
+    {
+      "id": "2",
+      "Todo": "My Data",
+    },
+    {
+      "id": "3",
+      "Todo": "My Data",
+    },
+    {
+      "id": "4",
+      "Todo": "My Data",
+    },
+    {
+      "id": "1",
+      "Todo": "My Data",
+    },
+    {
+      "id": "2",
+      "Todo": "My Data",
+    },
+    {
+      "id": "3",
+      "Todo": "My Data",
+    },
+    {
+      "id": "4",
+      "Todo": "My Data",
+    },
+  ];
+
+  const Item = ({ Todo }) => (
+    <View style={styles.item}>
+      <Text style={styles.data}>{Todo}</Text>
+    </View>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.list}>
+        <Text style={styles.title}>Today's Task</Text>
+        <FlatList
+          data={Data}
+          renderItem={({ item }) => <Item Todo={item.Todo} />}
+          keyExtractor={item => item.id}
+          style={styles.flatlist}
+        />
+
+      </View>
+      <View style={styles.Todoicon}>
+        <Icon
+          name='pluscircleo'
+          size={50}
+          color='#000000'
+          style={styles.icon} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  // sectionContainer: {
-  //   marginTop: 32,
-  //   paddingHorizontal: 24,
-  // },
-  // sectionTitle: {
-  //   fontSize: 24,
-  //   fontWeight: '600',
-  // },
-  // sectionDescription: {
-  //   marginTop: 8,
-  //   fontSize: 18,
-  //   fontWeight: '400',
-  // },
-  // highlight: {
-  //   fontWeight: '700',
-  // },
+  container: {
+    flex: 1,
+  },
+  list: {
+    flexDirection: 'column',
+    // backgroundColor:'red'
+  },
+  Todoicon: {
+    // zIndex: 1,
+    // backgroundColor:'gray',
+    position: 'absolute',
+    bottom: 30,
+    right: 10,
+
+  },
+  icon: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
+
+  }, item: {
+    backgroundColor: '#d3d3d3',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10
+  },
+  data: {
+    fontSize: 20,
+  },
+  title: {
+    marginTop: 16,
+    fontSize: 26,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  flatlist: {
+    marginTop: 20,
+  }
 });
 
-export default App;
+export default TODO;
